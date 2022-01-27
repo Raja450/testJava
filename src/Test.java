26fd850fa90b3506251ee2edc17c9a2006c97644
Raja450 <69240883+Raja450@users.noreply.github.com>
2022-01-28 00:16:37 +0530

    Update Test.java
    deleted Code

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+	
 		System.out.println("Hello This is Updated in GitHuB..");
 		System.out.println("Hello this is updated from the Eclipse ");
 	}
